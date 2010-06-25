cf2f7dda2a6b6d7730af509de684eb4648964c5d
katschst@gmx.de <katschst@gmx.de@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-06-25 12:43:30 +0000

    Kommentare vervollständigt.

--- simplex4android/src/com/googlecode/simplex4android/SimplexHistory.java
+++ simplex4android/src/com/googlecode/simplex4android/SimplexHistory.java
@@ -3,45 +3,62 @@ package com.googlecode.simplex4android;
 import java.util.ArrayList;
 
 /**
- * Klasse SimplexHistory - speichert alle Tableaus, von der Eingabe bis zur optimalen Lösung.
+ * Klasse SimplexHistory - speichert SimplexProbleme.
  * @author
  */
 public class SimplexHistory {
 
+	/**
+	 * History zur Verwaltung von SimplexPrioblemen.
+	 */
 	private ArrayList<SimplexProblem> history = new ArrayList<SimplexProblem>();
 	
+	/**
+	 * Leerer Konstruktor zum Anlegen einer SimplexHistory.
+	 */
 	public SimplexHistory(){
 		
 	}
 	
+	/**
+	 * Legt eine SimplexHistory zur Verwaltung von SimplexProblemen.
+	 * @param tableau Start-SimplexProblem
+	 * @param target Zielfunktion
+	 */
 	public SimplexHistory(double[][] tableau, int[] target){
 		SimplexProblem firstProblem = new SimplexProblem(tableau, target);
 		history.add(firstProblem);
 	}
 	
 	/**
-	 * @return
+	 * Gibt das letzte Element der SimplexHistory zurück.
+	 * @return letztes Element der SimplexHistory
 	 */
 	public SimplexProblem getLastElement(){
 		return this.history.get(this.history.size()-1);
 	}
 	
 	/**
-	 * @return
+	 * Gibt das erste Element der SimplexHistory zurück.
+	 * @return erstes Element der SimplexHistory
 	 */
 	public SimplexProblem getFirstElement(){
 		return this.history.get(0);
 	}
 
 	/**
-	 * @return
+	 * Gibt das Element mit dem Index index zurück.
+	 * @param index Index des auszugebenen Elements
+	 * @return Element mit Index index
+	 * @throws java.lang.IndexOutOfBoundsException
 	 */
 	public SimplexProblem getElement(int index) throws java.lang.IndexOutOfBoundsException{
 		return this.history.get(index);
 	}
 	
-		/**
-	 * @param tableau
+	/**
+	 * Fügt der SimplexHistory ein neues Element hinzu.
+	 * @param tableau einzufügendes SimplexProblem
 	 */
 	public void addElement(SimplexProblem tableau){
 		this.history.add(tableau);
