26fe52157c046311f41d2b5802e19ff558f655b3
katschst@gmx.de <katschst@gmx.de@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-06-25 14:06:25 +0000

    Rumgespielt, Simplex funktioniert! (Feststellen, dass Optimum vorliegt fehlt noch!)

--- trunk/simplex4android/src/com/googlecode/simplex4android/Simplex.java
+++ trunk/simplex4android/src/com/googlecode/simplex4android/Simplex.java
@@ -1,5 +1,6 @@
 package com.googlecode.simplex4android;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 /**
@@ -9,42 +10,44 @@ import java.util.Arrays;
  */
 public class Simplex {
 	
-	SimplexHistory history;
-	
 	public Simplex(){
-		this.history = new SimplexHistory();
 	}
 	
 	public static void main(String[] args){
 		boolean debug = true;
-		Simplex simplex = new Simplex();
+		SimplexHistory sh = new SimplexHistory();
 		
 		//Settings lesen, SimplexSettings erzeugen
 		
 		//Eingabe lesen
-		double[][] tableau = {{-1.5,3,0,0,1,-1,6},{0,1,0,1,0,-1,3},{0.5,-1,1,0,0,1,1},{0,0,0,0,0,0,0}}; //Beispiel-Tableau
-		int[] target = {1,2,7,5,0,0,0}; //Beispiel-Zielfunktion - Zielfunktion muss um eine 0 verlängert werden, um Zielwert berechnen zu können!!!
+		//Beispiel-Tableau
+		double[][] tableau = {{-1.5,3,0,0,1,-1,6},{0,1,0,1,0,-1,3},{0.5,-1,1,0,0,1,1},{0,0,0,0,0,0,0}}; 
+		//Beispiel-Zielfunktion - Zielfunktion muss um eine 0 verlängert werden, um Zielwert berechnen zu können!!!
+		int[] target = {1,2,7,5,0,0,0}; 
 		
 		//SimplexProblem erzeugen (aus Tableau, Target, SimplexSettings)
 		SimplexProblem firstProblem = new SimplexProblem(tableau, target);
 			
 		//SimplexProblem in History einfügen
-		simplex.history.addElement(firstProblem);
+		sh.addElement(firstProblem);
+		firstProblem.setPivots(SimplexLogic.findPivots(firstProblem));
+		firstProblem = SimplexLogic.calcDeltas(firstProblem);
+		
+		if(debug == true){System.out.println("Tableau: \n" + firstProblem.tableauToString());} 
+		if(debug == true){System.out.println("Zielfunktion: " + firstProblem.targetToString());}
 
 		//SimplexLogic auf SimplexProblem(e) ausführen, bis optimale Lösung gefunden, dabei Ausgabe aller Zwischenschritte
-		//while(simplex.history.getLastElement().getOptimal() != true){
-			SimplexLogic current = new SimplexLogic(simplex.history.getLastElement());
-			simplex.history.addElement(current.simplex());
+		for(int i=0;i<2;i++){
+			SimplexProblem current = sh.getLastElement();
+			current = SimplexLogic.doSimplex(current);
 			
 			//Debug-Ausgabe
-			if(debug == true){System.out.println("Tableau: \n" + current.problem.tableauToString());} 
-			if(debug == true){System.out.println("Zielfunktion: " + current.problem.targetToString());} 
-			if(debug == true){System.out.println("Tableau (Delta-Werte berechnet): " + Arrays.deepToString(current.problem.getTableau()));} //Debug
-			if(debug == true){System.out.println("Basisspalten: " + Arrays.toString(current.problem.getPivots()));}
-			if(debug == true){System.out.println("Aktuell gewählte Pivotspalte: "+current.choosePivotRow());}
+			if(debug == true){System.out.println("Tableau: \n" + current.tableauToString());}
+			if(debug == true){System.out.println("Basisspalten: " + Arrays.toString(current.getPivots()));}
+			if(debug == true){System.out.println("Aktuell gewählte Pivotspalte: "+SimplexLogic.choosePivotRow(current));}
 			
-
-		//}
+			sh.addElement(current);
+		}
 	}
 
 	
