d11c2641bd6e42008d63ea220a14a4c3d017f4d3
sebastianhanschke@googlemail.com <sebastianhanschke@googlemail.com@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-06-29 19:44:39 +0000

    GUI

--- simplex4android/src/com/googlecode/simplex4android/simplex4android.java
+++ simplex4android/src/com/googlecode/simplex4android/simplex4android.java
@@ -2,26 +2,37 @@ package com.googlecode.simplex4android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebView;
+import android.widget.Button;
 
 public class simplex4android extends Activity {
 
-	WebView mWebView;
-	
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.view);
+	    setContentView(R.layout.main);
+	    
+	    final Button button = (Button) findViewById(R.id.button_new);
+	    button.setOnClickListener(new OnClickListener() {
+	        public void onClick(View v) {
+	            setContentView(R.layout.constraint);
+	        }
+	    });
 
-		double[][] tableau = {{-1.5,3,0,0,1,-1,6},{0,1,0,1,0,-1,3},{0.5,-1,1,0,0,1,1},{0,0,0,0,0,0,0}}; 
+	    
+	    //Testproblem
+	    double[][] tableau = {{-1.5,3,0,0,1,-1,6},{0,1,0,1,0,-1,3},{0.5,-1,1,0,0,1,1},{0,0,0,0,0,0,0}}; 
 		int[] target = {1,2,7,5,0,0,0}; 
 		SimplexProblem firstProblem = new SimplexProblem(tableau, target);
-		
-	    mWebView = (WebView) findViewById(R.id.tableau);
+
+		//WebView mWebView;
+	    //mWebView = (WebView) findViewById(R.id.tableau);
 	    //mWebView.setWebViewClient(new HelloWebViewClient());
 	    //mWebView.getSettings().setJavaScriptEnabled(true);
 
-	    String summary = firstProblem.tableauToHtml();
-	    mWebView.loadData(summary, "text/html", "utf-8");
+	    //String summary = firstProblem.tableauToHtml();
+	    //mWebView.loadData(summary, "text/html", "utf-8");
 	    // ... although note that there are restrictions on what this HTML can do.
 	    // See the JavaDocs for loadData() and loadDataWithBaseURL() for more info.
 
