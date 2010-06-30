7bedef716cb20f9a750686b28174a362a77a96ae
sebastianhanschke@googlemail.com <sebastianhanschke@googlemail.com@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-06-30 23:14:36 +0000

    Simplex.java durch SimplexLocal.java ersetzt, RunConfigs bitte anpassen!!! Activities Simplex.java und simplex4android.java defekt - http://developer.android.com/guide/appendix/faq/commontasks.html#opennewscreen - Prinzip so langsam verstanden - Umsetzung folgt morgen

--- trunk/simplex4android/src/com/googlecode/simplex4android/Simplex.java
+++ trunk/simplex4android/src/com/googlecode/simplex4android/Simplex.java
@@ -1,5 +1,12 @@
 package com.googlecode.simplex4android;
 
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import java.util.Arrays;
 
 /**
@@ -7,10 +14,93 @@ import java.util.Arrays;
  * von SimplexLogic lösen und sorgt für die Ausgabe.
  * @author Simplex4Android
  */
-public class Simplex {
+
+public class Simplex extends Activity{
+
+    private EditText mTitleText;
+    private EditText mBodyText;
+    private Long mRowId;
+    //private NotesDbAdapter mDbHelper;
 	
-	public Simplex(){
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //mDbHelper = new NotesDbAdapter(this);
+        //mDbHelper.open();
+
+        setContentView(R.layout.view);
+
+        mTitleText = (EditText) findViewById(R.id.title);
+        mBodyText = (EditText) findViewById(R.id.body);
+
+        Button confirmButton = (Button) findViewById(R.id.confirm);
+
+        mRowId = (savedInstanceState == null) ? null :
+            (Long) savedInstanceState.getSerializable(NotesDbAdapter.KEY_ROWID);
+		if (mRowId == null) {
+			Bundle extras = getIntent().getExtras();
+			mRowId = extras != null ? extras.getLong(NotesDbAdapter.KEY_ROWID)
+									: null;
+		}
+
+		populateFields();
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                setResult(RESULT_OK);
+                finish();
+            }
+
+        });
+    }
+
+    private void populateFields() {
+        if (mRowId != null) {
+            Cursor note = mDbHelper.fetchNote(mRowId);
+            startManagingCursor(note);
+            mTitleText.setText(note.getString(
+                    note.getColumnIndexOrThrow(NotesDbAdapter.KEY_TITLE)));
+            mBodyText.setText(note.getString(
+                    note.getColumnIndexOrThrow(NotesDbAdapter.KEY_BODY)));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        saveState();
+        outState.putSerializable(NotesDbAdapter.KEY_ROWID, mRowId);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveState();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        populateFields();
+    }
+
+    private void saveState() {
+        String title = mTitleText.getText().toString();
+        String body = mBodyText.getText().toString();
+
+        if (mRowId == null) {
+            long id = mDbHelper.createNote(title, body);
+            if (id > 0) {
+                mRowId = id;
+            }
+        } else {
+            mDbHelper.updateNote(mRowId, title, body);
+        }
+    }
+	
+	
+	
 	
 	public static void main(String[] args){
 		boolean debug = true;
