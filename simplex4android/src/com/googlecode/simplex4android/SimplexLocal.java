5391e1d8315b35616215ee455e7f853fc7e7df16
sebastianhanschke@googlemail.com <sebastianhanschke@googlemail.com@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-06-30 23:14:36 +0000

    Simplex.java durch SimplexLocal.java ersetzt, RunConfigs bitte anpassen!!! Activities Simplex.java und simplex4android.java defekt - http://developer.android.com/guide/appendix/faq/commontasks.html#opennewscreen - Prinzip so langsam verstanden - Umsetzung folgt morgen

---
+++ simplex4android/src/com/googlecode/simplex4android/SimplexLocal.java
@@ -0,0 +1,52 @@
+package com.googlecode.simplex4android;
+
+import java.util.Arrays;
+
+/**
+ * Klasse Simplex - legt eine History an, nimmt die Eingabe entgegen, verarbeitet diese, lässt das Problem je nach Settings 
+ * von SimplexLogic lösen und sorgt für die Ausgabe.
+ * @author Simplex4Android
+ */
+public class SimplexLocal {
+	
+	public SimplexLocal(){
+	}
+	
+	public static void main(String[] args){
+		boolean debug = true;
+		SimplexHistory sh = new SimplexHistory();
+		
+		//Settings lesen, SimplexSettings erzeugen
+		
+		//Eingabe lesen
+		//Beispiel-Tableau
+		double[][] tableau = {{-1.5,3,0,0,1,-1,6},{0,1,0,1,0,-1,3},{0.5,-1,1,0,0,1,1},{0,0,0,0,0,0,0}}; 
+		//Beispiel-Zielfunktion - Zielfunktion muss um eine 0 verlängert werden, um Zielwert berechnen zu können!!!
+		int[] target = {1,2,7,5,0,0,0}; 
+		
+		//SimplexProblem erzeugen (aus Tableau, Target, SimplexSettings)
+		SimplexProblem firstProblem = new SimplexProblem(tableau, target);
+
+		//SimplexProblem in History einfügen
+		sh.addElement(firstProblem);
+		
+		if(debug == true){System.out.println("Tableau: \n" + firstProblem.tableauToString());} 
+		if(debug == true){System.out.println("Zielfunktion: " + firstProblem.targetToString());}
+		if(debug == true){System.out.println("HTML: "+ firstProblem.tableauToHtml());}
+
+		//SimplexLogic auf SimplexProblem(e) ausführen, bis optimale Lösung gefunden, dabei Ausgabe aller Zwischenschritte
+		do{
+			SimplexProblem current = sh.getLastElement();
+			current = SimplexLogic.simplex(current);
+
+			//Debug-Ausgabe
+			if(debug == true){System.out.println("Tableau: \n" + current.tableauToString());}
+			if(debug == true){System.out.println("Basisspalten: " + Arrays.toString(current.getPivots()));}
+			if(debug == true){System.out.println("Optimal: "+current.getOptimal());}
+			if(debug == true){System.out.println("HTML: "+current.tableauToHtml());}
+			
+			sh.addElement(current);
+		}
+		while(sh.getLastElement().getOptimal()!=true);
+	}	
+}
--- simplex4android/src/com/googlecode/simplex4android/simplex4android.java
+++ simplex4android/src/com/googlecode/simplex4android/simplex4android.java
@@ -1,5 +1,8 @@
 package com.googlecode.simplex4android;
 
+import com.googlecode.simplex4android.Simplex;
+import com.googlecode.simplex4android.R;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,12 +10,109 @@ import android.view.View.OnClickListener;
 import android.webkit.WebView;
 import android.widget.Button;
 
-public class simplex4android extends Activity {
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
+public class simplex4android extends Activity {
+	
+/*	
+ * Startet entweder 
+		Activity um neues Problem anzulegen, oder 
+		ListView um Probleme zu laden
+			lädt View zum ändern/anlegen von Problemen mit gespeicherten Werten
+			
+			laden Ausgabe, Zurückbutton, um wieder auf den HomeScreen zurück zu gelangen, Speichern Button, um zur ListView zurück zu kehren
+*/	
+	
+    private static final int ACTIVITY_CREATE=0;
+    private static final int ACTIVITY_EDIT=1;
+
+    private static final int INSERT_ID = Menu.FIRST;
+    private static final int DELETE_ID = Menu.FIRST + 1;
+	
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.main);
-	    
+        registerForContextMenu(getListView());
+	}
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, INSERT_ID, 0, "Hinzufügen");
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch(item.getItemId()) {
+            case INSERT_ID:
+                createNote();
+                return true;
+        }
+
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+            ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, DELETE_ID, 0, "Löschen");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case DELETE_ID:
+                AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+                //mDbHelper.deleteNote(info.id);
+                //fillData();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void createNote() {
+        Intent i = new Intent(this, SimplexProblemEdit.class);
+        startActivityForResult(i, ACTIVITY_CREATE);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Intent i = new Intent(this, NoteEdit.class);
+        i.putExtra(NotesDbAdapter.KEY_ROWID, id);
+        startActivityForResult(i, ACTIVITY_EDIT);
+    }
+	
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        //fillData();
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 	    final Button button = (Button) findViewById(R.id.button_new);
 	    button.setOnClickListener(new OnClickListener() {
 	        public void onClick(View v) {
