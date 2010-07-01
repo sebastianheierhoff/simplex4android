784d4161d3cea35ef73ea5174b15462b3a858e84
sebastianhanschke@googlemail.com <sebastianhanschke@googlemail.com@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-01 10:48:11 +0000

    On my way to a working UI

---
+++ simplex4android/src/com/googlecode/simplex4android/ConstraintEdit.java
@@ -0,0 +1,12 @@
+package com.googlecode.simplex4android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConstraintEdit extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.constraint_edit);
+	}
+}
