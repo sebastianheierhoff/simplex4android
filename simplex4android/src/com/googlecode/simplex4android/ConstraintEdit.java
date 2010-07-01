ac4ab0db0f4074b3aeb19669db69bc3dcf2fd52e
sebastianhanschke@googlemail.com <sebastianhanschke@googlemail.com@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-01 19:31:53 +0000

    On my way to a working UI:  - Startseite fertig  - Zielfunktion-Eingabe: Tastatur hinzugefügt

--- simplex4android/src/com/googlecode/simplex4android/ConstraintEdit.java
+++ simplex4android/src/com/googlecode/simplex4android/ConstraintEdit.java
@@ -2,11 +2,29 @@ package com.googlecode.simplex4android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class ConstraintEdit extends Activity {
 
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.constraint_edit);
+	    getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 	}
+	    
+	    
+//		((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).getCurrentInputConnection()
+	    
+//	    try{
+//	    	KeyboardView mInputView = (KeyboardView) getLayoutInflater().inflate(R.layout.constraint_edit, null);
+//		    Keyboard simplexkeyboard = new Keyboard(this, R.layout.simplexkeyboard);
+//		    mInputView.setKeyboard(simplexkeyboard);
+//	    }
+//	    catch(Exception e){
+//	        Toast.makeText(ConstraintEdit.this, "Fehler", Toast.LENGTH_LONG).show();
+//	    }
+	
+	
+	
+	
 }
--- simplex4android/src/com/googlecode/simplex4android/simplex4android.java
+++ simplex4android/src/com/googlecode/simplex4android/simplex4android.java
@@ -1,27 +1,12 @@
 package com.googlecode.simplex4android;
 
-//import com.googlecode.simplex4android.Simplex;
 import com.googlecode.simplex4android.R;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.webkit.WebView;
 import android.widget.Button;
-
-import android.app.ListActivity;
-import android.content.Intent;
-import android.database.Cursor;
-import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ContextMenu.ContextMenuInfo;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-import android.widget.AdapterView.AdapterContextMenuInfo;
 
 public class simplex4android extends Activity {
 	
@@ -34,26 +19,38 @@ public class simplex4android extends Activity {
 			laden Ausgabe, Zurückbutton, um wieder auf den HomeScreen zurück zu gelangen, Speichern Button, um zur ListView zurück zu kehren
 */	
 	
-//    private static final int ACTIVITY_CREATE=0;
-//    private static final int ACTIVITY_EDIT=1;
-//
-//    private static final int INSERT_ID = Menu.FIRST;
-//    private static final int DELETE_ID = Menu.FIRST + 1;
-	
-	public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.main);
 	    
-    	final Button button = (Button) findViewById(R.id.button_new);
-	    button.setOnClickListener(new OnClickListener() {
+    	final Button button_new = (Button) findViewById(R.id.button_new);
+	    button_new.setOnClickListener(new OnClickListener() {
 	        public void onClick(View v) {
-	    	    Intent ConstraintEditIntent = new Intent();
-	    	    ConstraintEditIntent.setClassName("com.googlecode.simplex4android.simplex4android", "com.googlecode.simplex4android.ConstraintEdit");
-	    	    //myIntent.putExtra("com.android.samples.SpecialValue", "Hello, Joe!"); 
-	    	    startActivity(ConstraintEditIntent);   
+	            Intent ConstraintEditIntent = new Intent();
+	            ConstraintEditIntent.setClassName("com.googlecode.simplex4android", "com.googlecode.simplex4android.ConstraintEdit");
+	        	startActivity(ConstraintEditIntent);
 	        }
 	    });
-
+	    	    
+    	final Button button_load = (Button) findViewById(R.id.button_load);
+	    button_load.setOnClickListener(new OnClickListener() {
+	        public void onClick(View v) {
+	            Intent SimplexHistoryLoadIntent = new Intent();
+	            SimplexHistoryLoadIntent.setClassName("com.googlecode.simplex4android", "com.googlecode.simplex4android.SimplexHistoryLoad");
+	        	startActivity(SimplexHistoryLoadIntent);
+	        }
+	    });
+	    
+//    	final Button button_tutorial = (Button) findViewById(R.id.button_tutorial);
+//	    button_tutorial.setOnClickListener(new OnClickListener() {
+//	        public void onClick(View v) {
+//	            Intent TutorialIntent = new Intent();
+//	            TutorialIntent.setClassName("com.googlecode.simplex4android", "com.googlecode.simplex4android.Tutorial");
+//	        	startActivity(TutorialIntent);
+//	        }
+//	    });
+	    
 	    //registerForContextMenu(getListView());
 	}
 	
@@ -108,9 +105,15 @@ public class simplex4android extends Activity {
 //    }
 	
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        //fillData();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == PICK_CONTACT) {
+//            if (resultCode == RESULT_OK) {
+                //Cursor contact = getContentResolver().query(data.getData(), null, null, null, null);
+                //contact.moveToFirst();
+                //String name = contact.getString(contact.getColumnIndexOrThrow(People.NAME));
+                //Toast.makeText(this, getString(R.string.chosen) + " " + name, Toast.LENGTH_LONG).show();
+//            }
+//	      }
     }
 }    
 
