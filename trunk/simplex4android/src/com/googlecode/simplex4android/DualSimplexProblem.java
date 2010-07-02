40871971e4792628fe1e8e4dc1cd1ffc4df92ade
max.wortm@gmail.com <max.wortm@gmail.com@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-02 15:39:04 +0000

    neue Unterteilung

---
+++ trunk/simplex4android/src/com/googlecode/simplex4android/DualSimplexProblem.java
@@ -0,0 +1,87 @@
+package com.googlecode.simplex4android;
+
+import java.util.ArrayList;
+
+public class DualSimplexProblem extends SimplexProblem {
+
+	private ArrayList<Double> deltaByF; //Zeile unter dem Tableau für den dualen Simplex
+	
+	/**
+	 * Standardkonstruktor für ein leeres SimplexProblem zum anschließenden Hinzufügen der Zielfunktion und Nebenbedingungen.
+	 * Die Zeile der delta-Werte ist bereits enthalten.
+	 */
+	public DualSimplexProblem(){
+		super();
+		this.deltaByF = new ArrayList<Double>();
+	}
+	
+	/**
+	 * Stellt ein SimplexTableau inklusive Zielfunktion zur Verfügung.
+	 * @param tableau
+	 * @param target
+	 */
+	public DualSimplexProblem(double[][] tableau, int[] target){ 
+		super(tableau, target);
+		SimplexLogic.findPivots(this);
+	    SimplexLogic.calcDeltas(this);
+	    SimplexLogic.calcDeltaByF(this);
+	}
+	
+	/**
+	 * Gibt ein Array mit den x/f-Werten für jede Zeile zurück.
+	 * @return Array mit den x/f-Werten für jede Zeile
+	 */
+	public double[] getDeltaByF() {
+		return this.convertToDblArray(this.deltaByF);
+	}
+	
+	/**
+	 * Überschreibt die delta/f-Werte.
+	 * @param deltaByF neue delta/f-Werte
+	 */
+	public void setDeltaByF(double[] deltaByF) {
+		this.deltaByF = this.convertToDblArrayList(deltaByF);
+	}
+	
+	/**Methode die das komplette Simplex-Tableau für den dualen Simplex als HTML-String zurückgibt.
+	 * 
+	 * @return komplettes Tableau als Duales Problem als String in Html 
+	 */
+	public String tableauToHtml(){
+		String html = "\n<html>\n<body>\n<table border=1 CELLSPACING=0>\n";
+		//1. Zeile: Zielfunktion
+		html = html + "<tr>\n<td></td><td></td>";		// direkt inkl. zwei leeren Einträgen 
+		for(int i=0;i<target.size()-1;i++){
+			html = html + "<td>" + (Math.round(target.get(i)*100.)/100.) + "</td>";
+		}
+		html = html + "<td></td></tr>\n";
+		//2. Zeile: zwei Zeilen frei Durchnummerierung der Spalten + x +x/f
+		html = html + "<tr><td></td><td></td>";		// direkt inkl. zwei leeren Einträgen
+		for(int i=0;i<target.size()-1;i++){
+			html = html +"<td>"+ (i+1) +"</td>";
+		}
+		html = html + "<td>x</td>";
+		//ab der 3. Zeile: das eigentliche Tableau, die ersten beiden Spalten auch wie im Tableau + x/f
+		for(int i=0;i<tableau.size()-1;i++){			//so oft ausführen wie es Zeilen-1 im Tableau gibt
+			html = html + "<tr><td>"+ target.get(pivots.get(i))+"</td><td>" +(pivots.get(i)+1) +"</td>";
+			for(int j=0;j<tableau.get(0).size();j++){
+				html = html + "<td>" + (Math.round((tableau.get(i).get(j))*100.)/100.)+"</td>";
+			}
+			html = html + "</tr>\n";
+		}
+		// Letzte Zeile: extra behandlung für delta-Wert
+		html = html + "<tr><td></td><td></td>"; //inkl. zwei leerfelder
+		for(int i=0;i<tableau.get(0).size();i++){
+			html = html + "<td>" + (Math.round((tableau.get(tableau.size()-1).get(i))*100.)/100.) +"</td>";
+		}
+		html = html + "</tr>\n";
+		// allerletzte Zeile mit den delta/f-Werten
+		html = html + "<tr><td></td><td></td>"; //inkl. zwei leerfelder
+		for(int i=0;i<deltaByF.size();i++){
+			html = html + "<td>" + (Math.round((deltaByF.get(i)*100.)/100.)) +"</td>";
+		}
+		html = html + "</tr>\n";
+		html = html + "</table>\n</body>\n</html>";
+		return html;
+	}
+}
