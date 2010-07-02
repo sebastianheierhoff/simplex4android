8917ce52d946f3ffe69d7a2ca346a08a8e22f77e
sebastianhanschke@googlemail.com <sebastianhanschke@googlemail.com@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-02 16:14:35 +0000

    UI: Eingabe Zielfunktion

--- trunk/simplex4android/src/com/googlecode/simplex4android/ConstraintEdit.java
+++ trunk/simplex4android/src/com/googlecode/simplex4android/ConstraintEdit.java
@@ -6,6 +6,7 @@ import android.text.Selection;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,20 +17,41 @@ public class ConstraintEdit extends Activity {
 	    setContentView(R.layout.constraint_edit);
 	    getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 	
+	    EditText target = (EditText) findViewById(R.id.target);
+	    target.setOnFocusChangeListener(new OnFocusChangeListener(){
+	    	public void onFocusChange(View v, boolean b){
+	    		if(b==true){
+		    		findViewById(R.id.keyboard).setVisibility(View.VISIBLE);
+		    		findViewById(R.id.button_back).setVisibility(View.INVISIBLE);
+		    		findViewById(R.id.button_add).setVisibility(View.INVISIBLE);
+		    		findViewById(R.id.simplex4android_logo).setVisibility(View.INVISIBLE);
+	    		}
+	    		else{
+		    		findViewById(R.id.keyboard).setVisibility(View.INVISIBLE);
+		    		findViewById(R.id.button_back).setVisibility(View.VISIBLE);
+		    		findViewById(R.id.button_add).setVisibility(View.VISIBLE);
+		    		findViewById(R.id.simplex4android_logo).setVisibility(View.VISIBLE);
+	    		}
+	    	}
+		});
+
+//	    target.setOnClickListener(new OnClickListener() {
+//	    	public void onClick(View v){
+//	    		findViewById(R.id.keyboard).setVisibility(View.VISIBLE);
+//	    	}
+//	    });
 	    
 		//Zielfunktion: ausgrauen von <, <=, >=, >
 		int[] keyboardButtons = {	R.id.button_0, R.id.button_1, R.id.button_2, R.id.button_3, R.id.button_4, R.id.button_5,
 									R.id.button_6, R.id.button_7, R.id.button_8, R.id.button_9,
-									R.id.button_x1, R.id.button_x2, R.id.button_x3, R.id.button_x4, R.id.button_xi,
-									R.id.button_plus, R.id.button_minus, R.id.button_divide, R.id.button_decimal, R.id.button_eq,
-									R.id.button_gt, R.id.button_geq, R.id.button_lt, R.id.button_leq, R.id.button_max, R.id.button_min, R.id.button_backspace};
+									R.id.button_minus, R.id.button_divide, R.id.button_decimal};
 		
 		for(int i=0; i<keyboardButtons.length; i++){
 			Button[] buttons = new Button[keyboardButtons.length];
 			buttons[i] = (Button) findViewById(keyboardButtons[i]);
 		    buttons[i].setOnClickListener(new OnClickListener() {
 		        public void onClick(View v) {
-		        	EditText text = (EditText) findViewById(R.id.target);
+		        	EditText text = (EditText) findViewById(R.id.target_element);
 		        	String newtext = text.getText().toString(); 
 		        	//Verbesserungsmöglichkeit: Taste gedrückt?
 		        	if(v.getTag().equals("backspace")){
@@ -45,6 +67,14 @@ public class ConstraintEdit extends Activity {
 		        }
 		    });
 		}
+		
+    	final Button back = (Button) findViewById(R.id.button_back);
+	    back.setOnClickListener(new OnClickListener() {
+	        public void onClick(View v) {
+	        	finish();
+	        }
+	    });
+		
 	}	
 //		((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).getCurrentInputConnection()
 	    
