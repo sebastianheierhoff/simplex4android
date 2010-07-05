dcee00865c36dad365a00881d90ef72e0895cb94
sebastianhanschke@googlemail.com <sebastianhanschke@googlemail.com@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-05 21:22:15 +0000

    On my way to a working UI: ein riesen Schritt für mich, ein kleiner Schritt für die UI TODO: Backspace hinzufügen, Minus nur als erstes Zeichen, . und / nicht als erstes Zeichen, nicht als zweites Zeichen, falls erstes Zeichen ein -, leeren Button löschen

--- trunk/simplex4android/src/com/googlecode/simplex4android/simplex4android.java
+++ trunk/simplex4android/src/com/googlecode/simplex4android/simplex4android.java
@@ -2,9 +2,7 @@ package com.googlecode.simplex4android;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -13,25 +11,31 @@ public class simplex4android extends Activity {
 	
 /*	
  * Startet entweder 
-		Activity um neues Problem anzulegen, oder 
-		ListView um Probleme zu laden
-			lädt View zum ändern/anlegen von Problemen mit gespeicherten Werten
-			
-			laden Ausgabe, Zurückbutton, um wieder auf den HomeScreen zurück zu gelangen, Speichern Button, um zur ListView zurück zu kehren
-*/	
-    static final int PICK_CONTACT_REQUEST = 0;
-
-    protected void onActivityResult(int requestCode, int resultCode,
-            Intent data) {
-        if (requestCode == PICK_CONTACT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                // A contact was picked.  Here we will just display it
-                // to the user.
-                startActivity(new Intent(Intent.ACTION_VIEW, data));
-            }
+ *		Activity um neues Problem anzulegen, oder 
+ *		ListView um Probleme zu laden
+ *			lädt View zum ändern/anlegen von Problemen mit gespeicherten Werten
+ *			
+ *			laden Ausgabe, Zurückbutton, um wieder auf den HomeScreen zurück zu gelangen, Speichern Button, um zur ListView zurück zu kehren
+ */	
+    static final int CREATE_CONSTRAINT_REQUEST = 0;
+    static final int CREATE_TARGET_REQUEST = 1;
+    static final int VIEW_CONSTRAINT_REQUEST = 2;
+    
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch (requestCode) {
+            case CREATE_CONSTRAINT_REQUEST:
+                if (resultCode == RESULT_CANCELED){
+                } 
+                else {
+                	Intent constraintView = new Intent().setClassName("com.googlecode.simplex4android", "com.googlecode.simplex4android.ConstraintView");
+                	startActivity(constraintView);
+                }
+            default:
+                break;
         }
-    }	
-
+    }
+    
+    
 	@Override
     public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
@@ -39,18 +43,11 @@ public class simplex4android extends Activity {
 
 	    final Button button_new = (Button) findViewById(R.id.button_new);
 	    button_new.setOnClickListener(new OnClickListener() {
-	        public void onClick(View v) {
-
-	        	startActivityForResult(
-	                      new Intent(Intent.ACTION_PICK,
-	                      new Uri("content://contacts")),
-	                      PICK_CONTACT_REQUEST);
-	                 return true;
-	      	    
-	        	Intent ConstraintEditIntent = new Intent();
-	            ConstraintEditIntent.setClassName("com.googlecode.simplex4android", "com.googlecode.simplex4android.ConstraintEdit");
-	        	startActivityForResult(ConstraintEditIntent, requestCode);
-	        }
+	    	public void onClick(View v){
+	        	Intent ConstraintEditIntent = new Intent().setClassName("com.googlecode.simplex4android", "com.googlecode.simplex4android.ConstraintEdit");
+	        	ConstraintEditIntent.putExtra("target", true);
+	        	startActivityForResult(ConstraintEditIntent, CREATE_CONSTRAINT_REQUEST);
+	    	}
 	    });
 	    	    
     	final Button button_load = (Button) findViewById(R.id.button_load);
@@ -71,71 +68,8 @@ public class simplex4android extends Activity {
 //	        	startActivity(TutorialIntent);
 //	        }
 //	    });
-	    
-	    //registerForContextMenu(getListView());
+    
 	}
-	
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        super.onCreateOptionsMenu(menu);
-//        menu.add(0, INSERT_ID, 0, "Hinzufügen");
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-//        switch(item.getItemId()) {
-//            case INSERT_ID:
-//                createNote();
-//                return true;
-//        }
-//
-//        return super.onMenuItemSelected(featureId, item);
-//    }
-
-//    @Override
-//    public void onCreateContextMenu(ContextMenu menu, View v,
-//            ContextMenuInfo menuInfo) {
-//        super.onCreateContextMenu(menu, v, menuInfo);
-//        menu.add(0, DELETE_ID, 0, "Löschen");
-//    }
-
-//    @Override
-//    public boolean onContextItemSelected(MenuItem item) {
-//        switch(item.getItemId()) {
-//            case DELETE_ID:
-//                AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-//                //mDbHelper.deleteNote(info.id);
-//                //fillData();
-//                return true;
-//        }
-//        return super.onContextItemSelected(item);
-//    }
-
-//    private void createSimplexHistory() {
-//        Intent i = new Intent(this, SimplexProblemEdit.class);
-//        startActivityForResult(i, ACTIVITY_CREATE);
-//    }
-
-//    @Override
-//    protected void onListItemClick(ListView l, View v, int position, long id) {
-//        super.onListItemClick(l, v, position, id);
-//        Intent i = new Intent(this, NoteEdit.class);
-//        i.putExtra(NotesDbAdapter.KEY_ROWID, id);
-//        startActivityForResult(i, ACTIVITY_EDIT);
-//    }
-	
-    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (requestCode == PICK_CONTACT) {
-//            if (resultCode == RESULT_OK) {
-                //Cursor contact = getContentResolver().query(data.getData(), null, null, null, null);
-                //contact.moveToFirst();
-                //String name = contact.getString(contact.getColumnIndexOrThrow(People.NAME));
-                //Toast.makeText(this, getString(R.string.chosen) + " " + name, Toast.LENGTH_LONG).show();
-//            }
-//	      }
-    }
 }    
 
 	    
