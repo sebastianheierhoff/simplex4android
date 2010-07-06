27ed3b472e2f33b828af1f78e5c5cebb5a05f7ac
katschst@gmx.de <katschst@gmx.de@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-06 21:08:41 +0000

    Oberklasse Inputs eingefügt, Constraint und Target sind nun Subklassen Methoden ausgelagert in Inputs

--- simplex4android/src/com/googlecode/simplex4android/Constraint.java
+++ simplex4android/src/com/googlecode/simplex4android/Constraint.java
@@ -1,81 +1,28 @@
 package com.googlecode.simplex4android;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
 /**
  * Datenhaltungklasse zur Repräsentation von Nebenbedingungen.
  * Nebenbedingungen können in Gleichheits- oder Ungleichheitsform ("<=" und ">=") eingegeben werden. Direkt beim Erstellen werde sie in Gleichheitsform gebracht und mit den nötigen Schlupfvariablen versehen.
  * @author Simplex4Android
  *
  */
-public class Constraint {
-	private ArrayList<Double> values;
+public class Constraint extends Inputs{
 	private double targetValue;
 	private int sign;
 	
 	/**
-	 * Legt für ein übergebenes int-Array und ein Vergleichssymbol eine Nebenbedingung an.
-	 * @param c int-Array mit den a-Werten der Nebenbedingung, an letzter Stelle steht b
-	 * @param s Vergleichssymbol ("-1" enspricht "<=", "0" entspricht "=" und "1" entspricht ">=")
+	 * Standardkonstruktor
 	 */
-	public Constraint(double[] c, int sign){
-		this.values = convertToDblArrayList(c);
-		this.sign = sign;
-	}
-	
 	public Constraint() {
-		values = new ArrayList<Double>();
-	}
-
-	/**
-	 * Fügt der Nebenbedingung an letzter Stelle ein neues Element hinzu.
-	 * @param value hinzuzufügendes Element
-	 */
-	public void add(Double value){
-		this.values.add(value);
-	}
-
-	/**
-	 * Überführt das übergebene Array in eine ArrayList<Double>.
-	 * @param array zu überführendes Array
-	 * @return überführte ArrayList
-	 */
-	private ArrayList<Double> convertToDblArrayList(double[] array){
-		ArrayList<Double> arrayList = new ArrayList<Double>();
-		for(int i=0;i<array.length;i++){
-			arrayList.add(i,new Double(array[i]));
-		}
-		return arrayList;
+		super();
 	}
 	
 	/**
-	 * Liest einen als String übergebenen Bruch aus und gibt den dazugehörigen double-Wert zurück
-	 * @param s zu bearbeitender Bruch
-	 * @return double-Wert des Bruchs
-	 * @throws IOException falls kein "/" im übergebenen String enthalten
+	 * Gibt den Vergleichsoperator zurück.
+	 * @return Vergleichsoperator ("-1" enspricht "<=", "0" entspricht "=" und "1" entspricht ">=")
 	 */
-	public static double fractionToDbl(String s) throws IOException{
-		int slash = s.indexOf("/");
-		int firstDigit = 0;
-		int lastDigit = 0;
-		if(slash==-1){
-			throw new IOException("Kein '/' enthalten");
-		}
-		for(int i=slash-1;i>0;i--){ // Anfangsindex der Zahl vor dem "/" finden
-			if(Integer.valueOf(s.substring(i-1,i))>=0 && Integer.valueOf(s.substring(i-1,i))<=9){
-				firstDigit = i-1;
-			}
-		}
-		for(int i=slash+1;i<s.length();i++){
-			if(Integer.valueOf(s.substring(i,i+1))>=0 && Integer.valueOf(s.substring(i,i+1))<=9){
-				lastDigit = i;
-			}
-		}
-		double numerator = Integer.valueOf(s.substring(firstDigit, slash));
-		double denomerator = Integer.valueOf(s.substring(slash+1, lastDigit+1));
-		double dbl = numerator/denomerator;
-		return dbl;
+	public int getSign(){
+		return sign;
 	}
 		
 	/**
@@ -84,15 +31,6 @@ public class Constraint {
 	 */
 	public double getTargetValue(){
 		return this.targetValue;
-	}
-	
-	/**
-	 * Gibt den Wert der Nebenbedingung an Index j zurück.
-	 * @return Wert der Nebenbedingung an Index j
-	 * @throws IndexOutOfBoundsException falls (i<0 || i>(size()-1))
-	 */
-	public double getValue(int j) throws IndexOutOfBoundsException{
-		return this.values.get(j);
 	}
 	
 	/**
@@ -110,47 +48,9 @@ public class Constraint {
 	public void setTargetValue(double value){
 		this.targetValue = value;
 	}
-	
-	/**
-	 * Setzt den Wert an Index j auf den übergebenen double-Wert. Ist der Index nicht vorhanden, wir passend aufgefüllt.
-	 * @param j Index des zu setzenden Elements
-	 * @param value Wert, auf den das Element gestetzt werden soll.
-	 */
-	public void setValue(int i, double value){
-		if(i>=this.values.size()){
-			for(int j=this.values.size();j<=i;j++){
-				this.values.add(new Double(0));
-			}
-		}
-		this.values.set(i, value);
-	}
 
 	
-	/**
-	 * Gibt eine Stringdarstellung der x1..xn aus.
-	 * @return Stringdarstellung der x1..xn
-	 */
-	public String valuesToString(){
-		String re = "";
-		re += Math.round(this.values.get(0)*100.)/100. +"x1";
-		for(int i=1;i<this.values.size();i++){
-			if(this.values.get(i)<0){
-				re += " " + String.valueOf(Math.round(this.values.get(i)*100.)/100.) +"x" +(i+1);
-			}else{
-				re += " + " + String.valueOf(Math.round(this.values.get(i)*100.)/100.) +"x" +(i+1);
-			}			
-		}
-		return re;
-	}
 	
-	/**
-	 * Gibt eine Stringdarstellung der x1..xn aus.
-	 * @return Stringdarstellung der x1..xn
-	 */
-	public String valueToString(int i) throws IndexOutOfBoundsException{
-		String re = Math.round(this.values.get(i)*100)/100 +"x" +(i+1);
-		return re;
-	}
 
 	
 }
--- simplex4android/src/com/googlecode/simplex4android/Target.java
+++ simplex4android/src/com/googlecode/simplex4android/Target.java
@@ -1,35 +1,14 @@
 package com.googlecode.simplex4android;
 
-import java.util.ArrayList;
 
-public class Target{
-	private ArrayList<Double> values;
+public class Target extends Inputs{
 	private boolean minOrMax; //true bedeutet Minimierung, false bedeutet Maximierung
 	
 	/**
-	 * Legt für ein übergebenes double-Array und ein Vergleichssymbol eine Nebenbedingung an.
-	 * @param values double-Array mit der Zielfunktion
-	 * @param minOrMax false steht für Maximierung, true für Minimierung
+	 * Standardkonstruktor
 	 */
-	public Target(double[] values, boolean minOrMax) {
-		this.values = convertToDblArrayList(values);
-		this.minOrMax = minOrMax;
-	}
-	
-	/**
-	 * get-Methode für die Zielfunktion
-	 * @return Zielfunktion in einer ArrayList mit Doublen
-	 */
-	public ArrayList<Double> getValues() {
-		return values;
-	}
-	
-	/**
-	 * set-Methode für die Zielfunktion
-	 * @param values Zielfunktion als Arraylist mit Doublen
-	 */
-	public void setValues(ArrayList<Double> values) {
-		this.values = values;
+	public Target() {
+		super();
 	}
 	
 	/**
@@ -46,18 +25,5 @@ public class Target{
 	 */
 	public void setMinOrMax(boolean minOrMax) {
 		this.minOrMax = minOrMax;
-	}
-	
-	/**
-	 * Überführt das übergebene Array in eine ArrayList<Double>.
-	 * @param array zu überführendes Array
-	 * @return überführte ArrayList
-	 */
-	private ArrayList<Double> convertToDblArrayList(double[] array){
-		ArrayList<Double> arrayList = new ArrayList<Double>();
-		for(int i=0;i<array.length;i++){
-			arrayList.add(i,new Double(array[i]));
-		}
-		return arrayList;
 	}
 }
