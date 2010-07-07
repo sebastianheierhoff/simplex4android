888ec3d47599a0fcafbefb568d936d5accfb3c79
katschst@gmx.de <katschst@gmx.de@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-07 15:43:00 +0000

    TestInputs ersetzt TestConstraint nach Umbau der Constraint in Subklasse

---
+++ trunk/simplex4androidTest/src/com/googlecode/simplex4android/test/TestInputs.java
@@ -0,0 +1,41 @@
+package com.googlecode.simplex4android.test;
+
+import java.io.IOException;
+import com.googlecode.simplex4android.Constraint;
+import com.googlecode.simplex4android.Inputs;
+import junit.framework.TestCase;
+
+public class TestInputs extends TestCase {
+	Inputs c;
+	public static void setUpClass() throws Exception{
+	}
+	public static void tearDownClass() throws Exception{
+	}
+	public void setUp(){
+		c = new Constraint();
+	}
+	public void tearDown(){
+	}
+	
+	/**
+	 * Testet, ob das Hinzufügen eines neuen Elementes an einem noch nicht existenten Index funktioniert.
+	 */
+	public void testSetValue(){
+		c.setValue(5, new Double(5));
+		assertEquals(c.getValue(5), new Double(5).doubleValue());
+	}
+	
+	/**
+	 * Testet, ob ein der Methode fractionToDbl(String s) übergebener Bruch-String fehlerfrei in ein double überführt wird.
+	 */
+	public void testFractionToDbl(){
+		String s = "24/12";
+		double d = 24.0/12.0;
+		try{
+			assertEquals(d,Constraint.fractionToDbl(s));
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+}
