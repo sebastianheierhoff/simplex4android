0445009c76819001bfaa9f82f8865dbb50398bda
katschst@gmx.de <katschst@gmx.de@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-08 11:16:04 +0000

    On my way to a working UI: Bugfixes

--- trunk/simplex4android/src/com/googlecode/simplex4android/InputShow.java
+++ trunk/simplex4android/src/com/googlecode/simplex4android/InputShow.java
@@ -14,27 +14,26 @@ public class InputShow extends Activity{
 	ArrayList<Input> input = new ArrayList<Input>();
 
 	static final int CREATE_CONSTRAINT_REQUEST = 0;
+	static final int CREATE_TARGET_REQUEST = 1;
 
+	
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.input_show);
 	    
 	    if(this.getIntent().getBooleanExtra("create", false)){
-        	Intent ConstraintEditIntent = new Intent().setClassName("com.googlecode.simplex4android", "com.googlecode.simplex4android.ConstraintEdit");
-        	startActivityForResult(ConstraintEditIntent, CREATE_CONSTRAINT_REQUEST);
+        	Intent ConstraintEditIntent = new Intent().setClassName("com.googlecode.simplex4android", "com.googlecode.simplex4android.TargetEdit");
+        	startActivityForResult(ConstraintEditIntent, CREATE_TARGET_REQUEST);
 	    }
 	    if(this.getIntent().getBooleanExtra("edit", false)){
 	    
 	    }
 	
-	    
-	    
     	final Button back = (Button) findViewById(R.id.button_cancel);
 	    back.setOnClickListener(new OnClickListener() {
 	        public void onClick(View v) {
 	        	finish();
 	        }
 	    });
-	    
 	}
 }
