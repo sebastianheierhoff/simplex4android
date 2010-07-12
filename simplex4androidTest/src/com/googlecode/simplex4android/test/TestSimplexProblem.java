ad7b8a39a41c201faac02b97b751b05c09867495
katschst@gmx.de <katschst@gmx.de@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-12 21:58:09 +0000

    TestSimplexProblemPrimal gelöscht TestSimplexProblem zum Testen des Konstruktors, der eie ArrayList<Input> verarbeitet, und von addRow() eingefügt (Inhalt fehlt noch)

---
+++ simplex4androidTest/src/com/googlecode/simplex4android/test/TestSimplexProblem.java
@@ -0,0 +1,21 @@
+package com.googlecode.simplex4android.test;
+
+import com.googlecode.simplex4android.SimplexProblem;
+
+import junit.framework.TestCase;
+
+public class TestSimplexProblem extends TestCase {
+	public static void setUpClass() throws Exception{
+	}
+	public static void tearDownClass() throws Exception{
+	}
+	public void setUp(){
+	}
+	public void tearDown(){
+	}
+	
+	public void testAddRow(){
+		
+	}
+
+}
