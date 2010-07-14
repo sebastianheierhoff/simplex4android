1c3de8b31bc6ae8ae4809ab7c61d2dfb7055901c
katschst@gmx.de <katschst@gmx.de@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-14 14:52:06 +0000

    Target um userSetting für Benutzereinstellungen ergänzt.

--- simplex4android/src/com/googlecode/simplex4android/Target.java
+++ simplex4android/src/com/googlecode/simplex4android/Target.java
@@ -1,13 +1,26 @@
 package com.googlecode.simplex4android;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Target extends Input{
-	private boolean minOrMax; //true bedeutet Minimierung, false bedeutet Maximierung
+	private boolean minOrMax;	// true bedeutet Minimierung, false bedeutet Maximierung
+	private int userSettings; 	// legt die vom Benutzer gewünschten Settings für das Lösen eines SimplexProblems fest
+								// 0=primal, 1=dual;
 	
 	/**
 	 * Standardkonstruktor
 	 */
 	public Target() {
 		super();
+	}
+	
+	/**
+	 * Gibt die vom Benutzer gewünschten Settings für das Lösen eines SimplexProblems zurück.
+	 * @return 0=primal, 1=dual
+	 */
+	public int getUserSettings(){
+		return this.userSettings;
 	}
 	
 	/**
@@ -24,6 +37,24 @@ public class Target extends Input{
 	 */
 	public void setMinOrMax(boolean minOrMax) {
 		this.minOrMax = minOrMax;
+	}
+	
+	/**
+	 * Setzt die vom Benutzer gewünschten Settings für das Lösen eines SimplexProblems.
+	 * @param setting 0=primal, 1=dual
+	 * @throws IOException falls ungültiger Wert gesetzt werden soll.
+	 */
+	public void setUserSettings(int setting) throws IOException{
+		ArrayList<Integer> settings = new ArrayList<Integer>();
+		for(int i=0;i<2;i++){
+			settings.add(new Integer(i));
+		}
+		if(settings.contains(new Integer(setting))){
+			this.userSettings = setting;
+		}else{
+			throw new IOException("Ungültige Settings!");
+		}
+		
 	}
 	
 	/**
