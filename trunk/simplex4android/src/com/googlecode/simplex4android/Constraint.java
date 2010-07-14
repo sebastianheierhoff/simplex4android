a324c7fbef3188ab645635c0f73e4e1b14dd9d1a
sebastianhanschke@googlemail.com <sebastianhanschke@googlemail.com@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-14 21:47:52 +0000

    InputsDB

--- trunk/simplex4android/src/com/googlecode/simplex4android/Constraint.java
+++ trunk/simplex4android/src/com/googlecode/simplex4android/Constraint.java
@@ -1,8 +1,9 @@
 package com.googlecode.simplex4android;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.util.ArrayList;
 
 /**
  * Datenhaltungklasse zur Repräsentation von Nebenbedingungen.
@@ -10,26 +11,18 @@ import java.util.ArrayList;
  * @author Simplex4Android
  *
  */
+@SuppressWarnings("serial")
 public class Constraint extends Input implements Serializable{
-	private static final long serialVersionUID = -4853003520885704436L; // NICHT ÄNDERN!
-
-	/**
-	 * @serial
-	 */
 	private double targetValue;
-
-	/**
-	 * @serial
-	 */
 	private int sign;
-
+	
 	/**
 	 * Standardkonstruktor
 	 */
 	public Constraint() {
 		super();
 	}
-
+	
 	/**
 	 * Gibt den Vergleichsoperator zurück.
 	 * @return Vergleichsoperator ("-1" enspricht "<=", "0" entspricht "=" und "1" entspricht ">=")
@@ -37,7 +30,7 @@ public class Constraint extends Input implements Serializable{
 	public int getSign(){
 		return sign;
 	}
-
+		
 	/**
 	 * Gibt den Zielfunktionswert zurück.
 	 * @return Zielfunktionswert
@@ -45,7 +38,7 @@ public class Constraint extends Input implements Serializable{
 	public double getTargetValue(){
 		return this.targetValue;
 	}
-
+	
 	/**
 	 * Setzt das Vergleichssymbol und daraufhin die ggf. benötigte Schlupfvariable, um die Nebenbedingung in Standardform zu bringen.
 	 * @param s "-1" enspricht "<=", "0" entspricht "=" und "1" entspricht ">="
@@ -53,7 +46,7 @@ public class Constraint extends Input implements Serializable{
 	public void setSign(int s){
 		this.sign = s;
 	}
-
+	
 	/**
 	 * Setzt den Zielfunktionswert.
 	 * @param value zu setzender Zielfunktionswert
@@ -61,7 +54,7 @@ public class Constraint extends Input implements Serializable{
 	public void setTargetValue(double value){
 		this.targetValue = value;
 	}
-
+	
 	/**
 	 * Gibt die Nebenbedingung (komplett) als String aus.
 	 */
@@ -79,52 +72,28 @@ public class Constraint extends Input implements Serializable{
 		return s;
 	}
 
-	/**
-	 * Always treat de-serialization as a full-blown constructor, by
-	 * validating the final state of the de-serialized object.
-	 */
-	private void readObject(java.io.ObjectInputStream aInputStream) throws ClassNotFoundException, IOException{
-		aInputStream.defaultReadObject();
-
-		//make defensive copy of the mutable Date field
-		this.values = new ArrayList<Double>(values);
-
-
-		//ensure that object state has not been corrupted or tampered with maliciously
-	}
 
 	/**
-	 * This is the default implementation of writeObject.
-	 * Customise if necessary.
-	 */
-	private void writeObject(java.io.ObjectOutputStream aOutputStream) throws IOException {
-		//perform the default serialization for all non-transient, non-static fields
-		aOutputStream.defaultWriteObject();
-	}
+	  * Always treat de-serialization as a full-blown constructor, by
+	  * validating the final state of the de-serialized object.
+	  */
+	   private void readObject(
+	     ObjectInputStream aInputStream
+	   ) throws ClassNotFoundException, IOException {
+	     //always perform the default de-serialization first
+	     aInputStream.defaultReadObject();
+	     //make defensive copy of the mutable Date field
+	     //fDateOpened = new Date( fDateOpened.getTime() );
+	  }
 
-	//	
-	//	/**
-	//	 * Schreibt die Constraint in den gewünschten OutputStream.
-	//	 * @param out OutputStream, in den geschrieben werden soll.
-	//	 * @throws IOException
-	//	 */
-	//	private void writeObject(java.io.ObjectOutputStream out) throws IOException{
-	//		out.writeDouble(this.targetValue);
-	//		out.writeInt(this.sign);
-	//		out.writeObject(this.values.toArray());
-	//		out.flush();
-	//		out.close();
-	//	}
-	//	
-	//	/**
-	//	 * Liest die Constraint aus dem InputStream.
-	//	 * @param in zu lesender InputStream
-	//	 * @throws IOException
-	//	 * @throws ClassNotFoundException
-	//	 */
-	//	private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException{
-	//		this.targetValue = in.readDouble();
-	//		this.sign = in.readInt();
-	//		this.values = this.convertToDblArrayList((double[]) in.readObject());
-	//	}
+	    /**
+	    * This is the default implementation of writeObject.
+	    * Customise if necessary.
+	    */
+	    private void writeObject(
+	      ObjectOutputStream aOutputStream
+	    ) throws IOException {
+	      //perform the default serialization for all non-transient, non-static fields
+	      aOutputStream.defaultWriteObject();
+	    }
 }
