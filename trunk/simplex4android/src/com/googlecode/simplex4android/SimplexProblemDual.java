c1d238a6261027192549fc3e00d8e0ef89c65557
max.wortm <max.wortm@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-16 18:27:36 +0000

    - bei der Anzeige von deltaByF<0 wie auch in den Folien

--- trunk/simplex4android/src/com/googlecode/simplex4android/SimplexProblemDual.java
+++ trunk/simplex4android/src/com/googlecode/simplex4android/SimplexProblemDual.java
@@ -130,9 +130,8 @@ public class SimplexProblemDual extends SimplexProblem {
 		// allerletzte Zeile mit den delta/f-Werten
 		html = html + "<tr align=right><td></td><td></td>"; //inkl. zwei leerfelder
 		for(int i=0;i<deltaByF.size();i++){
-			//if(deltaByF.get(i)>0)
-				html = html + "<td>" + (Math.round(deltaByF.get(i)*100.)/100.) +"</td>";
-			//else html = html + "<td>&#8211;</td>";
+			if(deltaByF.get(i)>0)html = html + "<td>" + (Math.round(deltaByF.get(i)*100.)/100.) +"</td>";
+			else html = html + "<td>&#8211;</td>";
 		}
 		html = html + "</tr>\n";
 		html = html + "</table>\n</body>\n</html>";
