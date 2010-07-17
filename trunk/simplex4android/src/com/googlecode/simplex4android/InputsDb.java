d0726428669fffd7fcbf0b56aa82a9480a230d1f
katschst@gmx.de <katschst@gmx.de@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-17 19:22:59 +0000

    Kommentare eingefügt.

--- trunk/simplex4android/src/com/googlecode/simplex4android/InputsDb.java
+++ trunk/simplex4android/src/com/googlecode/simplex4android/InputsDb.java
@@ -19,39 +19,82 @@ public class InputsDb {
 
 	private ArrayList<ArrayList<Input>> listOfInputs;//Liste zum Speichern von Problemen
 
+	/**
+	 * Konstruktor zum Anlegen einer InputsDb für einen übergebenen Kontext.
+	 * @param context aktueller Kontext
+	 * @throws StreamCorruptedException
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
 	public InputsDb(Context context) throws StreamCorruptedException, IOException, ClassNotFoundException{
 		listOfInputs = new ArrayList<ArrayList<Input>>();
 		this.readFile(context);
 	}
 	
+	/**
+	 * Setzt die gespeicherten Probleme.
+	 * @param listOfInputs zu speichernde Probleme
+	 */
 	public void setListOfInputs(ArrayList<ArrayList<Input>> listOfInputs){
 		this.listOfInputs = listOfInputs;
 	}
 	
+	/**
+	 * Gibt die gespeicherten Probleme zurück.
+	 * @return gespeicherte Probleme
+	 */
 	public ArrayList<ArrayList<Input>> getListOfInputs(){
 		return this.listOfInputs;
 	}
-	
+	/**
+	 * Fügt den gespeicherten Problemen ein weiteres hinzu und speichert direkt alle in eine Datei.
+	 * @param context aktueller Kontext
+	 * @param input zu speicherndes Problem (an Index i muss ein Objekt der Klasse Target stehen)
+	 * @throws Exception
+	 */
 	public void addInput(Context context, ArrayList<Input> input) throws Exception{
 		listOfInputs.add(input);
 		this.writeFile(context);
 	}
 
+	/**
+	 * Überschreibt das Problem an Index i der gespeicherten Probleme.
+	 * @param context aktueller Kontext
+	 * @param i Index i
+	 * @param input zu setzendes Problem
+	 * @throws Exception
+	 */
 	public void setInput(Context context, int i, ArrayList<Input> input) throws Exception{
 		this.listOfInputs.set(i, input);
 		this.writeFile(context);
 	}
 	
+	/**
+	 * Enternt das Problem an Index i
+	 * @param context aktueller Kontext
+	 * @param position Index i des zu entfernenden Problems
+	 * @throws Exception
+	 */
 	public void removeInput(Context context, int position) throws Exception{
 		this.readFile(context);
 		listOfInputs.remove(position);
 		this.writeFile(context);
 	}
 	
+	/**
+	 * Gibt das Problem an Index i zurück.
+	 * @param i 
+	 * @return Problem an Index i
+	 */
 	public ArrayList<Input> getInput(int i){
 		return listOfInputs.get(i);	
 	}
 	
+	/**
+	 * Gibt die Namen (Stringrepräsentationen der Zielfunktionen) der Probleme zurück.
+	 * @return Stringrepräsentationen der Zielfunktionen der Probleme
+	 * @throws NegativeArraySizeException
+	 */
 	public String[] getNames() throws NegativeArraySizeException{
 		String[] s = new String[listOfInputs.size()-1];
 		for(int i=0;i<listOfInputs.size();i++){
@@ -60,6 +103,11 @@ public class InputsDb {
 		return s;
 	}
 	
+	/**
+	 * Schreibt die aktuelle Liste der Probleme in die Datei simplexProblems.dat.
+	 * @param context aktueller Kontext
+	 * @throws Exception 
+	 */
 	public void writeFile(Context context) throws Exception {
 		try{
 			context.getFileStreamPath("simplexProblems.dat").delete();
@@ -75,6 +123,13 @@ public class InputsDb {
 		oos.close();
 	}
 
+	/**
+	 * Liest eine Liste von Problemen aus der Datei simplexProblems.dat.
+	 * @param context aktueller Kontext
+	 * @throws StreamCorruptedException
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
 	@SuppressWarnings("unchecked")
 	public void readFile(Context context) throws StreamCorruptedException, IOException, ClassNotFoundException{
 		FileInputStream fis = null;
