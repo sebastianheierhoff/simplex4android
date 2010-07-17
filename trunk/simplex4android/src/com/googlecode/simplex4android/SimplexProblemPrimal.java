e78129a7a7183d61b49cd2febaf9150afc50d0d4
max.wortm <max.wortm@da072d13-551f-a551-a6f6-b8d7ad0cb193>
2010-07-17 15:03:57 +0000

    Zeilenumbruch komplett abgeschaft

--- trunk/simplex4android/src/com/googlecode/simplex4android/SimplexProblemPrimal.java
+++ trunk/simplex4android/src/com/googlecode/simplex4android/SimplexProblemPrimal.java
@@ -94,18 +94,18 @@ public class SimplexProblemPrimal extends SimplexProblem {
 		//1. Zeile: Zielfunktion
 		html = html + "<tr align=right>\n<td></td><td></td>";		// direkt inkl. zwei leeren Einträgen 
 		for(int i=0;i<this.getTarget().length-1;i++){
-			html = html + "<td>" + (Math.round(this.getTarget()[i]*100.)/100.) + "</td>";
+			html = html + "<td nowrap>" + (Math.round(this.getTarget()[i]*100.)/100.) + "</td>";
 		}
 		html = html + "<td></td><td></td></tr>\n";
 		//2. Zeile: zwei Zeilen frei Durchnummerierung der Spalten + x +x/f
 		html = html + "<tr align=right><td></td><td></td>";		// direkt inkl. zwei leeren Einträgen
 		for(int i=0;i<this.getTarget().length-1;i++){
-			html = html +"<td>"+ (i+1) +"</td>";
+			html = html +"<td nowrap>"+ (i+1) +"</td>";
 		}
-		html = html + "<td>x</td><td>x/f</td>";
+		html = html + "<td nowrap>x</td><td nowrap>x/f</td>";
 		//ab der 3. Zeile: das eigentliche Tableau, die ersten beiden Spalten auch wie im Tableau + x/f
 		for(int i=0;i<this.getTableau().length-1;i++){			//so oft ausführen wie es Zeilen-1 im Tableau gibt
-			if(pivots.length <= this.getNoPivots())html = html + "<tr align=right><td>"+ this.getTarget()[pivots[i]]+"</td><td>" +(pivots[i]+1) +"</td>";
+			if(pivots.length <= this.getNoPivots())html = html + "<tr align=right><td nowrap>"+ this.getTarget()[pivots[i]]+"</td><td nowrap>" +(pivots[i]+1) +"</td>";
 			else html = html + "<tr align=right><td>"+"&#8211;"+"</td><td>" +"&#8211;"+"</td>";
 			for(int j=0;j<this.getTableau()[0].length;j++){
 				if(SimplexLogic.choosePivotRow(this)==i && SimplexLogic.choosePivotColumn(this)==j){
@@ -131,8 +131,8 @@ public class SimplexProblemPrimal extends SimplexProblem {
 		// Letzte Zeile: extra behandlung für delta-Wert
 		html = html + "<tr align=right><td></td><td></td>"; //inkl. zwei leerfelder
 		for(int i=0;i<this.getTableau()[0].length;i++){
-			if(pivots.length <= this.getNoPivots())html = html + "<td>" + (Math.round((this.getTableau()[(this.getTableau().length-1)][i])*100.)/100.) +"</td>";
-			else html = html + "<td>" + "&#8211;" +"</td>";
+			if(pivots.length <= this.getNoPivots())html = html + "<td nowrap>" + (Math.round((this.getTableau()[(this.getTableau().length-1)][i])*100.)/100.) +"</td>";
+			else html = html + "<td nowrap>" + "&#8211;" +"</td>";
 		}
 		html = html + "<td></td></tr>\n";
 		html = html + "</table>\n</body>\n</html>";
